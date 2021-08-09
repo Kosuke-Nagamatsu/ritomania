0ad30903320a1f1be7fb9e401387195bd05a3f4a
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-09 14:16:42 +0900

    追加：日本語化のためデフォルトロケールを設定し、gemをインストール

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module OriginalApp
     config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.assets false
       g.helper false
