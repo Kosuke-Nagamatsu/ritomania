8f0f81f1ed00e679060b5677e75a021c9b02f521
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-09 16:15:07 +0900

    追加：iconオブジェクト作成時にデフォルト画像を設定するようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
   has_one_attached :icon
   validates :title, length: { in: 1..30 }
+  before_create :default_icon
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def default_icon
+    if !self.icon.attached?
+      self.icon.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_icon.jpg')), filename: 'default_icon.jpg', content_type: 'image/jpg')
+    end
+  end
 end
