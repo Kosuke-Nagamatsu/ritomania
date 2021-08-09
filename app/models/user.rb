a2de3f84d4a87db9c0bbd42d23d143def8c4367b
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-09 16:06:46 +0900

    追加：nameカラムにlengthのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :icon
+  validates :title, length: { in: 1..30 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
