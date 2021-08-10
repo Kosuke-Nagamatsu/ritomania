c4b2579ebdf8d4936627f991e9ae81195bf2d0aa
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-10 11:33:00 +0900

    追加：postsコントローラにアソシエーションとストロングパラメータに関して追記

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
@@ -64,6 +64,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:content, :prefecture)
+      params.require(:post).permit(:content, :prefecture, :image)
     end
 end
