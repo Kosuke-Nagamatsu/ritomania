958765fabb80b2cb414e468dd5e3064f06277f4d
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-11 15:50:16 +0900

    追加：commentsコントローラ　createアクションとprivateメソッドを実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: [:create, :edit, :update]
   def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    respond_to do |format|
+      if @comment.save
+        format.js { render :index }
+      else
+        format.html { redirect_to post_path(@post), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+  private
+  def comment_params
+    params.require(:comment).permit(:post_id, :content)
+  end
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
