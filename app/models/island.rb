da06b733341ccb4e604541cc0de36f1ddaa842f8
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-13 05:02:13 +0900

    追加：Island,Userモデルそれぞれのアソシエーション設定を追記・修正。アカウント登録時、チェックボックスを使用することで好きな離島を複数登録できるようにした

--- app/models/island.rb
+++ app/models/island.rb
@@ -2,4 +2,5 @@ class Island < ApplicationRecord
   has_many :post_islands, dependent: :destroy
   has_many :posts, through: :post_islands, source: :post
   has_many :island_favorites, dependent: :destroy
+  has_many :users, through: :islands_favorites
 end
