1176c56345f9288c2b75a9446df1bb8b06f7e903
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-12 12:42:19 +0900

    追加：お試しで離島名のseedデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+islands = ["波照間島", "石垣島", "座間味島", "宮古島", "父島", "母島"]
+islands.each do |island|
+  Island.create!(name: island)
+end
