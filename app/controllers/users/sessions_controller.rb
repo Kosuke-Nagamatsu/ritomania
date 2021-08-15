7ca362bbd04da83233881ec7cbc7b7970b0e7fae
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-15 11:29:38 +0900

    追加：sessionsコントローラにゲストログインの処理を記述

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,2 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
+  def guest_sign_in
+    user = User.find_or_create_by!(email: 'guest-user@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to posts_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
 end
