de64f8e6cdeb9177a2852dfb496ed25dd0634396
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-15 11:23:18 +0900

    追加：ゲストサインインアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'comments/create'
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   resources :users, only: [:show]
   resources :posts do
     collection do
