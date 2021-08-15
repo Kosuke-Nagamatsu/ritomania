8daf7f86073581116e45563d48c0df430275a342
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-15 14:09:38 +0900

    追加：管理者ゲストユーザを探す or 作成するメソッドを追記。一般ゲストのメアドを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,13 @@ class User < ApplicationRecord
   end
 
   def self.guest
-    find_or_create_by!(email: 'guest-user@example.com') do |user|
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
+
+  def self.admin_guest
+    find_or_create_by!(email: 'admin-guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
   end
