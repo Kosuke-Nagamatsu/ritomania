4b0ae4f20c74902608afed4b8e1bbe32296184f5
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-17 16:00:10 +0900

    追加：他人の投稿・プロフィールが編集できないようコントローラにアクセス制限を追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show ]
   def show
+    redirect_to user_path(current_user.id) unless @user == current_user
   end
   private
   def set_user
