b57f2e565f6d9aeb6b760cfb02581a00d36255d0
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-23 19:22:23 +0900

    追加：本番環境でletter_opener_webを使用できるように設定を修正・追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.action_mailer.default_url_options = { host: 'https://limitless-beyond-47766.herokuapp.com/' }
+  config.action_mailer.delivery_method = :letter_opener_web
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :favorites, only: [:index]
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
