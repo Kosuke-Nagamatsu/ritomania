4350bf1055e69f7ebe1d8814ef5c596897ec5a24
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-25 22:53:39 +0900

    テスト：Postモデルのテストが完了

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,19 +1,48 @@
 require 'rails_helper'
 describe 'ポストモデル機能', type: :model do
   describe 'バリデーションのテスト' do
-    before do
-    end
-    context 'ポストの本文が空の場合' do
+    context 'ポストの写真が未選択の場合' do
       it 'バリデーションにひっかる' do
-        post = FactoryBot.create(:post)
-        # post.image = fixture_file_upload("files/test.png")
+        user = FactoryBot.create(:user)
+        post = Post.create( image: nil, content: "離島へいこう！", prefecture: "沖縄県", user_id: user.id )
         expect(post).not_to be_valid
       end
     end
-  # context 'ポストの都道府県が空の場合' do
-  #     it 'バリデーションにひっかかる' do
-  #       # ここに内容を記載する
-  #     end
-  #   end
+    context 'ポストの本文が空の場合' do
+      it 'バリデーションにひっかる' do
+        user = FactoryBot.create(:user)
+        post = Post.create(
+          image: ActiveStorage::Blob.create_after_upload!(io: File.open("#{Rails.root}/spec/fixtures/images/test.png"), filename: 'test.png'),
+          content: "",
+          prefecture: "沖縄県",
+          user_id: user.id
+        )
+        expect(post).not_to be_valid
+      end
+    end
+    context 'ポストの都道府県が空の場合' do
+      it 'バリデーションにひっかかる' do
+        user = FactoryBot.create(:user)
+        post = Post.create(
+          image: ActiveStorage::Blob.create_after_upload!(io: File.open("#{Rails.root}/spec/fixtures/images/test.png"), filename: 'test.png'),
+          content: "離島へいこう！",
+          prefecture: "",
+          user_id: user.id
+        )
+        expect(post).not_to be_valid
+      end
+    end
+    context 'ポストの写真を選択し、本文・都道府県に内容が入っている場合' do
+      it 'バリデーションが通る' do
+        user = FactoryBot.create(:user)
+        post = Post.create(
+          image: ActiveStorage::Blob.create_after_upload!(io: File.open("#{Rails.root}/spec/fixtures/images/test.png"), filename: 'test.png'),
+          content: "離島へいこう！",
+          prefecture: "沖縄県",
+          user_id: user.id
+        )
+        expect(post).to be_valid
+      end
+    end
   end
 end
