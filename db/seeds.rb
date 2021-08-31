d22e8a5bfacf19b9719592a659a2072abbf55cc4
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-08-31 12:50:24 +0900

    追加：シードデータを微修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -146,24 +146,24 @@ post_island.save!
 #   content: "あの奥に見える橋はなんですか？"
 # )
 # comment.save!
-# comment = Comment.new(
-#   user_id: User.find(3).id,
-#   post_id: Post.find(4).id,
-#   content: "あれは〇〇です！"
-# )
-# comment.save!
-# comment = Comment.new(
-#   user_id: User.find(1).id,
-#   post_id: Post.find(4).id,
-#   content: "へー。そうなんですね。。めっちゃ綺麗なんで絶対いきます！"
-# )
-# comment.save!
-# comment = Comment.new(
-#   user_id: User.find(3).id,
-#   post_id: Post.find(4).id,
-#   content: "せひぜひ〜"
-# )
-# comment.save!
+comment = Comment.new(
+  user_id: User.find(3).id,
+  post_id: Post.find(4).id,
+  content: "あれは〇〇です！"
+)
+comment.save!
+comment = Comment.new(
+  user_id: User.find(1).id,
+  post_id: Post.find(4).id,
+  content: "へー。そうなんですね。。めっちゃ綺麗なんで絶対いきます！"
+)
+comment.save!
+comment = Comment.new(
+  user_id: User.find(3).id,
+  post_id: Post.find(4).id,
+  content: "せひぜひ〜"
+)
+comment.save!
 comment = Comment.new(
   user_id: User.find(6).id,
   post_id: Post.find(6).id,
