cd8d6f5fdb77f023a829eeb40d0d4bbefbbec20f
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-09-01 23:15:03 +0900

    追加：遷移元のURLを取得し、'/posts'が含まれる場合は@current_pageにindexを代入しviewへ渡すよう追記

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -11,5 +11,11 @@ class FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
     favorite.destroy
+
+    if request.referer&.include?('/posts')
+      respond_to do |format|
+        format.js { @current_page = "index" }
+      end
+    end
   end
 end
