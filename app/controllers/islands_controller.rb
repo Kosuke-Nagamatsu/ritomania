c1efac75f1f63d6e6dd4f5a93f1c8cfec50f2789
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-09-18 12:33:15 +0900

    追加：islandsコントローラに処理を記述

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,4 +1,12 @@
 class IslandsController < ApplicationController
+  before_action :set_post, only: %i[show]
   def show
+    @comment = @post.islands.params[:id]
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
