87df54f870b6526bc7e4933885a4b623ec9634ca
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-09-23 23:32:58 +0900

    追加：.envに書いた環境変数（APIキー）とテーブルから取りだしたシティIDをjs内で使えるよう追記

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,6 +1,9 @@
 class IslandsController < ApplicationController
   before_action :set_island, only: %i[show]
   def show
+    gon.api_key = ENV["OPEN_WEATHER_MAP_API_KEY"]
+    gon.city_id = @island.city_id
+    binding.pry
   end
 
   private
